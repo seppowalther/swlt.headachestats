4721d393f064b3942b8fe1d94a6690f62d0acf36
seppowalther <seppo.walther@educorvi.de>
2021-03-02 01:02:17 +0100

    Preparing data for further data processing

--- venv/src/main.py
+++ venv/src/main.py
@@ -31,8 +31,29 @@ month = splittet[1]
 year = splittet[2]
 date = datetime.datetime(int(year), int(month), int(day))
 
-# Filling factor-IDs:
+"""
+AVAILABLE factor-IDs:
 
+wetter = ['sonnig','bewoelkt','regnerisch','neblig']
+hoechsttemperatur = ['>35', '30-35', '20-30', '10-20', '5-10', '0-5', '<0']
+zeit_aussen = ['>3', '>2', '>1', '0-1', '0']
+sport = ['indoor', 'outdoor', 'no']
+getrunken = ['>3,5', '2,5-3,5', '2-2,5', '<2']
+medikamente = ['ibuprofen', 'paracetamol', 'aspirin', 'neuralgin']
+"""
+
+# Preparing CSV-Output-factors for further data processing
+if testlist_splitted[2] == "Sonnig":
+    wetter = 'sonnig'
+elif testlist_splitted[2] == "Bewölkt":
+    wetter = 'bewoelkt'
+elif testlist_splitted[2] == "Regnerisch":
+    wetter = 'regnerisch'
+elif testlist_splitted[2] == "Neblig":
+    wetter = 'neblig'
+
+
+# Filling factor-IDs:
 date = date
 hoehepunkt_kopfschmerz = 0
 durchschnitt_kopfschmerz = 0
@@ -40,13 +61,3 @@ hoehepunkt_nackenschmerzen = 0
 durchschnitt_nackenschmerzen = 0
 durchschnitt_stresspiegel = 0
 durchschnitt_gesamtstimmung = 0
-wetter = ['sonnig','bewoelkt','regnerisch','neblig']
-hoechsttemperatur = ['>35', '30-35', '20-30', '10-20', '5-10', '0-5', '<0']
-zeit_aussen = ['>3', '>2', '>1', '0-1', '0']
-sport = ['indoor', 'outdoor', 'no']
-getrunken = ['>3,5', '2,5-3,5', '2-2,5', '<2']
-medikamente = ['ibuprofen', 'paracetamol', 'aspirin', 'neuralgin']
-
-#
-
-print(date)
