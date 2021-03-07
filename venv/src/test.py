f16bcf0d679c1b956784786aa4ae8da6ded6f39c
seppowalther <seppo.walther@educorvi.de>
2021-03-07 22:26:54 +0100

    Fixing bug

--- venv/src/test.py
+++ venv/src/test.py
@@ -14,12 +14,13 @@ dataset = alldata[1]
 # Filling factor-IDs:
 
 owndatadict = dict()
-owndatadict[dataset[1]] = dataset[1]
-owndatadict[dataset[1]]["wetter"]
+owndatadict[dataset[1]] = {}
+owndatadict[dataset[1]]["date"] = dataset[1]
+owndatadict[dataset[1]]["hoehepunkt_kopfschmerz"] = dataset[2]
+#owndatadict[dataset[1]]["durchschnitt_kopfschmerz"] = dataset[3]
+#owndatadict[dataset[1]]["wetter"] =
 #owndatadict[dataset[1]]["durchschnitt_stresspiegel"]
 #owndatadict[dataset[1]]["durchschnitt_gesamtstimmung"]
-#owndatadict[dataset[1]]["hoehepunkt_kopfschmerz"]
-#owndatadict[dataset[1]]["durchschnitt_kopfschmerz"]
 #owndatadict[dataset[1]]["getrunken"]
 #owndatadict[dataset[1]]["hoechsttemperatur"]
 #owndatadict[dataset[1]]["zeit_aussen"]
